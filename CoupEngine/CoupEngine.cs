64779d49258e4d6a483e7f546744d84cf78a6b28
Eric Edmond <ericed@microsoft.com>
2019-11-30 10:26:03 -0800

    Add OtherPlayers enumerable to game engine

--- CoupEngine/CoupEngine.cs
+++ CoupEngine/CoupEngine.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,6 +10,19 @@ namespace CoupEngine
         public List<Player> PlayerList { get; private set; }
         public int ActivePlayerIndex { get; private set; } = 0;
         public Player ActivePlayer { get { return PlayerList[ActivePlayerIndex]; } }
+        public IEnumerable<Player> OtherPlayers
+        {
+            get
+            {
+                // Returns all players other than the active player, in turn order
+                // starting from the player after the active player
+                for (int i = 1; i < PlayerList.Count; ++i)
+                {
+                    int playerIndex = (ActivePlayerIndex + i) % PlayerList.Count;
+                    yield return PlayerList[playerIndex];
+                }
+            }
+        }
 
         private Dictionary<TurnActionType, Turn> turnLookup;
 
