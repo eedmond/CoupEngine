dd0282d601b8626c007a31c760c557d3645dc4ac
Eric Edmond <ericed@microsoft.com>
2019-11-30 09:38:06 -0800

    Initialize repo

---
+++ CoupEngine/CoupEngine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoupEngine
+{
+    class CoupEngine
+    {
+        TurnManager turnManager;
+
+        public void PlayGame()
+        {
+            while (turnManager.NumPlayersRemaining > 1)
+            {
+                TurnActionType turn = turnManager.NextPlayer().ChooseNextAction();
+
+            }
+        }
+    }
+}
---
+++ CoupEngine/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CoupEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CoupEngine engine = new CoupEngine();
+            engine.PlayGame();
+        }
+    }
+}
