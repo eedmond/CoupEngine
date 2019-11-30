ee0527f0eaae820b0e0632881f4b1573a092c9f8
Eric Edmond <ericed@microsoft.com>
2019-11-30 12:50:49 -0800

    Begin implementing PlayerProcess

--- CoupEngine/PlayerProcess.cs
+++ CoupEngine/PlayerProcess.cs
@@ -2,20 +2,31 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CoupEngine
 {
     internal class PlayerProcess
     {
+        private enum ProcessType
+        {
+            Exe,
+            Python,
+            Javascript
+        }
+
         public PlayerProcess(string processString)
         {
+            ProcessType processType = ParseProcessType(processString);
+
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.CreateNoWindow = true;
             startInfo.RedirectStandardInput = true;
             startInfo.RedirectStandardOutput = true;
             startInfo.UseShellExecute = false;
-            startInfo.Arguments = processString; //"PythonApp.py";
-            startInfo.FileName = "C:\\Program Files (x86)\\Microsoft Visual Studio\\Shared\\Python37_64\\python.exe";
+
+            SetStartArguments(startInfo, processString, processType);
 
             Process p = new Process();
             p.StartInfo = startInfo;
@@ -28,6 +39,46 @@ namespace CoupEngine
             p.Start();
             p.BeginOutputReadLine();
             p.WaitForExit();
+        }
+
+        private ProcessType ParseProcessType(string processString)
+        {
+            var stringPieces = processString.Split('.');
+            var fileExtension = stringPieces[stringPieces.Length - 1];
+
+            switch (fileExtension.ToLower())
+            {
+                case "exe":
+                    return ProcessType.Exe;
+
+                case "js":
+                    return ProcessType.Javascript;
+
+                case "py":
+                    return ProcessType.Python;
+            }
+
+            throw new ArgumentException();
+        }
+
+        private void SetStartArguments(ProcessStartInfo startInfo, string processString, ProcessType processType)
+        {
+            switch (processType)
+            {
+                case ProcessType.Exe:
+                    startInfo.FileName = processString;
+                    break;
+
+                case ProcessType.Javascript:
+                    startInfo.FileName = "node";
+                    startInfo.Arguments = processString;
+                    break;
+
+                case ProcessType.Python:
+                    startInfo.FileName = "pythong";
+                    startInfo.Arguments = processString;
+                    break;
+            }
         }
     }
 }
