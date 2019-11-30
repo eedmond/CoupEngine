e58c30c8d2e5b7a838920acf5d41eb3b79fb62a3
iblaauw <iblaauw@umich.edu>
2019-11-30 14:04:17 -0800

    Added a preliminary protocol for communicating with the process

--- CoupEngine/Player.cs
+++ CoupEngine/Player.cs
@@ -6,6 +6,34 @@ using System.Text;
 
 namespace CoupEngine
 {
+    // Full list of our protocol:
+    // Sending: Every time we send something, the first word designates what happened,
+    // then comes any details (who played it, etc), finally ended by a newline.
+    // If we are expecting a response, the last thing before the newline should be a '?' character
+    // 
+    // Serialization for our actions (details excluded)
+    // - AM : ambassador
+    // - AS : assassinate
+    // - CH! : challenge
+    // - COUP : coup
+    // - FA : Foreign Aid
+    // - IN : Income
+    // - CLAIM : Role claim
+    // - CAP : captain / steal
+    //
+    // Serialization for requests:
+    // - R? : request for a 'response' to an action another player took (challenge or claim)
+    // - L? : losing a life, request which card they want to reveal (this doesn't happen if they only have one card)
+    // - A? : request for what action they want to take (they are the active player now)
+    //
+    // Serialization for reponses should be either a serialization for one of the actions above. If the action targets a player,
+    // just put the player number ('PlayerId') as a second word.
+    // For the response to 'L?' it should be the abbreviated name of a role, which are listed as above, or in addition:
+    // - CONT : Contessa
+    // - DUKE : Duke
+    //
+    // Note that all requests/responses should be considered case-insensitive: the process doesn't have to care about receiving/outputing 'AM' vs 'am' vs 'Am'
+
     internal class Player
     {
         public void LoseLife(CoupEngine engine)
@@ -21,7 +49,8 @@ namespace CoupEngine
 
         public void NotifyAction(GameAction action, Player actingPlayer)
         {
-            throw new NotImplementedException();
+            // TODO: actually send string
+            string toSend = action.SerializeAction();
         }
 
         public ResponseAction GetResponse()
@@ -33,7 +62,6 @@ namespace CoupEngine
         private void LoseLifeInternal(CoupEngine engine, bool loseRole1)
         {
             var otherPlayers = engine.OtherPlayers(this);
-            // TODO: when a role is lost, reveal it on the table - this is fun because it needs to have access to the engine
             if (loseRole1)
             {
                 // Tell the other players what the role we lost was
@@ -56,6 +84,7 @@ namespace CoupEngine
             else
             {
                 Debug.Assert(role2.HasValue);
+
                 // Tell the other players what the role we lost was
                 foreach (var player in otherPlayers)
                 {
