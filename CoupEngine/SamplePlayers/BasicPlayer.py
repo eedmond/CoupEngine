bbb4b3e72d9c8cfc9fc78c4007ac61f33d27a7b1
Eric Edmond <ericed@microsoft.com>
2019-11-30 18:14:24 -0800

    Get a working python player

--- CoupEngine/SamplePlayers/BasicPlayer.py
+++ CoupEngine/SamplePlayers/BasicPlayer.py
@@ -8,14 +8,16 @@ class Role(Enum):
    Contessa = 3
    Duke = 4
 
-isAlive = true
+isAlive = True
 
 def main():
+    global isAlive
+
     args = input().split()
     assert((args[0].lower() == 'glhf') and (len(args) == 5));
 
-    myId = args[1]
-    numPlayers = args[2]
+    myId = int(args[1])
+    numPlayers = int(args[2])
     activePlayers = range(numPlayers)
     role1 = parse_role(args[3])
     role2 = parse_role(args[4])
@@ -33,16 +35,22 @@ def main():
 
             elif nextInput[0].lower() == 'a':
                 if (money < 7):
-                    print ('i')
+                    print ("IN")
+                    money += 1
 
                 else:
                     playerToCoup = activePlayers[0]
                     if (activePlayers[0] == myId):
                         playerToCoup = activePlayers[1]
-                    print ("c " + playerToCoup)
+                    print ("COUP " + str(playerToCoup))
+                    money -= 7
 
-        elif nextInput[0].lower().startswith("rip"):
-            killedPlayer = nextInput[4]
+        elif nextInput.lower().startswith("el"):
+            isAlive = False
+        elif nextInput.lower().startswith("gg"):
+            isAlive = False
+        elif nextInput.lower().startswith("rip"):
+            killedPlayer = int(nextInput[4])
             activePlayers = [player for player in activePlayers if player != killedPlayer]
             
 
